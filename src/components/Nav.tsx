989fe1b9e89c437c7cf7d0fd27ce4f808f987076
BorisovecViktor <viktorborisovets@gmail.com>
2020-05-28 15:08:55 +0300

    fixed header & added footer

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,32 +1,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Nav = () => {
-  const headerLinks: HeaderLink[] = [
-    { title: 'HOME', url: '/#' },
-    { title: 'PHONES', url: '/phones' },
-    { title: 'TABLETS', url: '/tablets' },
-    { title: 'ACCESSORIES', url: '/accessories' },
-  ]
-  return (
-    <nav className="header__nav nav">
-      <ul className="nav__list">
-        {headerLinks.map(({ title, url }) => (
-          <li
-            className="nav__item"
-            key={title}>
-            <NavLink
-              className="nav__link"
-              activeClassName="nav__link--active"
-              to={url}>
-              {title}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
+type Props = {
+  links: NavLinkType[],
 }
 
-export default Nav;
+const Nav: React.FC<Props> = ({ links }) => (
+  <nav className="nav">
+    <ul className="nav__list">
+      {links.map(({ title, url }) => (
+        <li
+          className="nav__item"
+          key={title}>
+          <NavLink
+            className="nav__link"
+            exact
+            activeClassName="nav__link--active"
+            to={url}>
+            {title}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
 
+export default Nav;
