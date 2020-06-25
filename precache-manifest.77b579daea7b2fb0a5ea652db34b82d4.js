c592c1a9cc06eb5cc9a80849c52461c637a53db1
BorisovecViktor <viktorborisovets@gmail.com>
2020-06-25 10:08:02 +0300

    Updates

---
+++ precache-manifest.77b579daea7b2fb0a5ea652db34b82d4.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a26973e4025ab126052172e64b449c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "848d58997ccca708527f",
+    "url": "./static/css/main.248e8f9e.chunk.css"
+  },
+  {
+    "revision": "68b546880cce519f408a",
+    "url": "./static/js/2.2dec3201.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.2dec3201.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "848d58997ccca708527f",
+    "url": "./static/js/main.4f5f5800.chunk.js"
+  },
+  {
+    "revision": "1610068e25a4810046d0",
+    "url": "./static/js/runtime-main.e1f50cee.js"
+  },
+  {
+    "revision": "482e37260cd688576287f1976e958e0b",
+    "url": "./static/media/Mont-Bold.482e3726.otf"
+  },
+  {
+    "revision": "f0f845588eef439ac6ecc9b6ab42191c",
+    "url": "./static/media/Mont-Regular.f0f84558.otf"
+  },
+  {
+    "revision": "d41a520b1802421f24e21279736e1871",
+    "url": "./static/media/Mont-SemiBold.d41a520b.otf"
+  },
+  {
+    "revision": "b96cc7d79bf73bf4f0673de0bd10c044",
+    "url": "./static/media/arrow-down.b96cc7d7.svg"
+  },
+  {
+    "revision": "c04f58ba855a95a0fea25837001a3e4e",
+    "url": "./static/media/breadcrumb-arrow.c04f58ba.svg"
+  },
+  {
+    "revision": "bbe701a798719d996b7e9325607159d2",
+    "url": "./static/media/breadcrumb-home.bbe701a7.svg"
+  },
+  {
+    "revision": "c8a5446121f3152382ee24a14922bc33",
+    "url": "./static/media/cart-default.c8a54461.svg"
+  },
+  {
+    "revision": "d138e731cc776c15ecafc7eb28700475",
+    "url": "./static/media/cart-empty.d138e731.svg"
+  },
+  {
+    "revision": "e4ee3912eff45a2daf3cae0c5968033d",
+    "url": "./static/media/check-circle.e4ee3912.svg"
+  },
+  {
+    "revision": "6f553d93f2b3773340b890adab9ca954",
+    "url": "./static/media/close.6f553d93.svg"
+  },
+  {
+    "revision": "ba064880a8e6bce13a241bd23737f156",
+    "url": "./static/media/favorite_default.ba064880.svg"
+  },
+  {
+    "revision": "6da4f5192e45db4de6412d3569a2a464",
+    "url": "./static/media/footer-top-arrow.6da4f519.svg"
+  },
+  {
+    "revision": "fc2574b435c86fbc0ecd9c51b90be43b",
+    "url": "./static/media/goback-arrow.fc2574b4.svg"
+  },
+  {
+    "revision": "2a6877cc203b14548aa89db969519cc0",
+    "url": "./static/media/heart-break.2a6877cc.svg"
+  },
+  {
+    "revision": "0ff1f79ea40edea460f445b1f3752486",
+    "url": "./static/media/heart-empty.0ff1f79e.svg"
+  },
+  {
+    "revision": "5402f79f60ab5c6838c7cf36f411ab91",
+    "url": "./static/media/heart-full.5402f79f.svg"
+  },
+  {
+    "revision": "5193249bd8100c912438c84be92a667a",
+    "url": "./static/media/minus.5193249b.svg"
+  },
+  {
+    "revision": "e5d09faa2a5e4d7d3f3f2c26fdfffba5",
+    "url": "./static/media/person-empty-dolly.e5d09faa.svg"
+  },
+  {
+    "revision": "f998a5f5afa4496e5903c66d2e7333df",
+    "url": "./static/media/plus.f998a5f5.svg"
+  },
+  {
+    "revision": "d5f941ebd8ca6134fbbbae1e936f6a34",
+    "url": "./static/media/sad-cry.d5f941eb.svg"
+  },
+  {
+    "revision": "bdce88035724b4def5b7f9cfe14eabc2",
+    "url": "./static/media/search.bdce8803.svg"
+  },
+  {
+    "revision": "5ca5c1f58aa3bcefb2bf88439be8fe51",
+    "url": "./static/media/slider-arrow-left.5ca5c1f5.svg"
+  },
+  {
+    "revision": "67be4a4a4d709745ad41a3578d953895",
+    "url": "./static/media/slider-arrow-right.67be4a4a.svg"
+  },
+  {
+    "revision": "078c49d7a3498951ed1020131f048477",
+    "url": "./static/media/сarousel-arrow.078c49d7.svg"
+  }
+]);
